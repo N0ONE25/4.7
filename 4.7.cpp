1645f6723d150abf2a05bb1f6d1f3b62b4aae8b9
N0ONE25 <valvladgonext@gmail.com>
2023-10-25 01:06:05 +0300

    4.7

---
+++ 4.7.cpp
@@ -0,0 +1,48 @@
+// Lab_04_7.cpp
+// < Зозюк Влад >
+// Лабораторна робота № 4.7
+// Обчислення суми ряду Тейлора за допомогою ітераційних циклів та рекурентних співвідношень
+// Варіант 8
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double xp, xk, x, dx,eps, a = 0, R = 0, S = 0;
+	int n = 0; 
+
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "eps = "; cin >> eps;
+	cout << fixed;
+	cout << "-------------------------------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(10) << "atan" << " |"
+		<< setw(7) << "S" << " |"
+		<< setw(5) << "n" << " |"
+		<< endl;
+	cout << "-------------------------------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+	
+		n = 0;
+		S = 0;
+		do {
+			n++;
+			a = (pow(-1,n+1)) / ((2*n+1)*pow(x,2*n+1));
+			S += a;
+		} while (a >= eps);
+
+		cout << "|" << setw(7) << setprecision(2) << x << " |"
+			<< setw(10) << setprecision(5) << atan(x)  << " |"
+			<< setw(10) << setprecision(5) << 4 * atan(1.0) / 2  * S    << " |"
+			<< setw(5) <<                n             << " |"
+			<< endl;             
+		x += dx;
+	}
+	cout << "-------------------------------------------------" << endl;
+	return 0;
+}
